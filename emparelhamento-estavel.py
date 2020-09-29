08c42edd605e03501160227bf89e4ed1c1821bdf
Vitor Fraga <vitor.augusto.fraga@gmail.com>
2020-09-28 23:30:09 -0300

    ajustes

--- emparelhamento-estavel.py
+++ emparelhamento-estavel.py
@@ -4,27 +4,32 @@ import random
 
  # Referencia do matchmaker  "http://rosettacode.org/wiki/Stable_marriage_problem#Python"
 
-def matchmaker():
-    guysfree = guys[:]
+def tinder(mans, manschoice, womans, womanschoice):
+
+    guysfree = mans
     engaged  = {}
-    guyprefers2 = copy.deepcopy(guyprefers)
-    galprefers2 = copy.deepcopy(galprefers)
+    guyprefers2 = manschoice
+    galprefers2 = womanschoice
+
     while guysfree:
+
         guy = guysfree.pop(0)
         guyslist = guyprefers2[guy]
         gal = guyslist.pop(0)
         fiance = engaged.get(gal)
+
         if not fiance:
             # She's free
             engaged[gal] = guy
-            print("  %s and %s" % (guy, gal))
+           
         else:
             # The bounder proposes to an engaged lass!
             galslist = galprefers2[gal]
+
             if galslist.index(fiance) > galslist.index(guy):
                 # She prefers new guy
                 engaged[gal] = guy
-                print("  %s dumped %s for %s" % (gal, fiance, guy))
+               
                 if guyprefers2[fiance]:
                     # Ex has more girls to try
                     guysfree.append(fiance)
@@ -34,6 +39,8 @@ def matchmaker():
                     # Look again
                     guysfree.append(guy)
     return engaged
+
+
 
 def get_dataset(start = None, end = None):
     
@@ -59,3 +66,12 @@ def get_dataset(start = None, end = None):
     
     
     return f
+
+
+df = get_dataset(1,10)
+mans = list(df['H'].keys())
+womans = list(df['M'].keys())
+manschoice = df['H']
+womanschoice = df['M']
+engaged = tinder(mans=mans, manschoice=manschoice, womans=womans, womanschoice=womanschoice)
+print(engaged)
